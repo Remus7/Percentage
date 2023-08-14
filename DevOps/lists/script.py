a7ccbebed5d25354b100c818ad856833b095f9b8
DynoW <104089032+DynoW@users.noreply.github.com>
2023-08-14 17:25:19 +0300

    modified:   DevOps/lists/script.py

--- DevOps/lists/script.py
+++ DevOps/lists/script.py
@@ -9,13 +9,13 @@ f = open('Cocktails.json')
 cocktails = json.load(f)
 
 for cocktail in cocktails:
-    print(cocktail)
-    r.hmset(cocktail, {"glass": cocktails[cocktail]["glass"], "ingredients": json.dumps(cocktails[cocktail]["ingredients"]), "preparation": cocktails[cocktail]["preparation"]})
+    cocktail2 = cocktail.lower()
+    r.hmset(cocktail2, {"glass": cocktails[cocktail]["glass"], "ingredients": json.dumps(cocktails[cocktail]["ingredients"]), "preparation": cocktails[cocktail]["preparation"]})
 
 g = open('Ingredients.json')
 
 ingredients = json.load(g)
 
 for ingredient in ingredients:
-    ingredient = ingredient.lower()
-    r.hset("INGREDIENTS", ingredient, json.dumps(ingredients[ingredient]))
+    ingredient2 = ingredient.lower()
+    r.hset("INGREDIENTS", ingredient2, json.dumps(ingredients[ingredient]))
