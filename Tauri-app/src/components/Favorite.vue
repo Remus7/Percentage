45a15fae480716e9880e15a5fe717ab7c046db06
iLaurian <91423981+iLaurian@users.noreply.github.com>
2023-08-15 02:10:12 +0300

    favoriteDrinks export

--- Tauri-app/src/components/Favorite.vue
+++ Tauri-app/src/components/Favorite.vue
@@ -1,8 +1,44 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import { favoriteDrinks } from "../App.vue";
+
+function removeFavorite(index: Number){
+    favoriteDrinks.value.splice(index, 1);
+}
+
 </script>
 
 <template>
 
+<button class="favorite-drink" v-for="(drink, index) in favoriteDrinks" :key="drink">
+    {{ drink }}
+    <button class="remove" @click="removeFavorite(index)">X</button>
+</button>
+
 </template>
+
+<style>
+.favorite-drink {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 8px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+.remove {
+  background-color: #f44336;
+  color: white;
+  border: none;
+  border-radius: 50%;
+  padding: 4px 8px;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+.remove:hover {
+  background-color: #d32f2f;
+}
+
+</style>
