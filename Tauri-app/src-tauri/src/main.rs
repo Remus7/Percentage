4143cdadcd201af1ddcf17515a801ca9b2335407
AlexCrominus <antal7349@gmail.com>
2023-08-14 22:24:43 +0300

    modified:   Tauri-app/src-tauri/src/main.rs 	modified:   Tauri-app/src/example.json

--- Tauri-app/src-tauri/src/main.rs
+++ Tauri-app/src-tauri/src/main.rs
@@ -10,7 +10,8 @@ use serde_json::{Value, Map};
 use std::{thread, time};
 #[allow(unused_imports)]
 use tauri::Window;
-
+use serde_json::from_str;
+use std::collections::HashMap;
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
@@ -18,11 +19,22 @@ pub enum CommandError{
     Error(String)
 }
 
-async fn request_value(_url: String) -> Result<Value, CommandError>{
+use serde::{Deserialize};
+
+#[derive(Debug, Deserialize)]
+struct Cocktail {
+    glass: String,
+    ingredients: Vec<String>,
+    preparation: String,
+}
+
+async fn request_value(_url: String) -> Result<HashMap<String,Cocktail>, CommandError>{
     // let response = reqwest::get(url).await.map_err(|err| CommandError::Error(format!("{:?}", err)))?.text().await.map_err(|err| CommandError::Error(format!("{:?}", err)))?;
     let response = std::fs::read_to_string("../src/example.json").map_err(|err| CommandError::Error(format!("{:?}", err)))?;
 
-    Ok( serde_json::from_str(&response).map_err(|_err| CommandError::Error("Unable to unwrap to Value".to_owned()))? )
+    let cocktails: std::collections::HashMap<String, Cocktail> = serde_json::from_str(&response).unwrap();
+
+    Ok(cocktails)
 }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -31,12 +43,11 @@ async fn drink_from_ingredients(ingredient_vec: Vec<String>) -> Result< Vec<Stri
     println!("{:?}",ingredient_vec);
     for i in 0..ingredient_vec.len(){
         let url = format!("REQUESTUL"); 
-        let drinks = request_value(url).await?;
-        println!("{}", drinks[i]);
-        for j in drinks.into_iter(){
-            println!("muie badea");
-            let ingr = j["ingredients"];
-            for k in 0..ingr.as_array().unwrap().len(){
+        let drinks: std::collections::HashMap<String, Cocktail>= request_value(url).await?;
+        for (name, cocktail) in drinks.into_iter(){
+            println!("{}",name);
+            let ingr = cocktail.ingredients;
+            for k in 0..ingr.len(){
                 println!("{:?}", ingr[k]);
             }
         }
