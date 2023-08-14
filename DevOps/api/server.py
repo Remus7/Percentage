aa14a7722091687072777413ad8b4505fb4668c7
AlexCrominus <antal7349@gmail.com>
2023-08-14 22:28:57 +0300

    reverted back to dic version 	modified:   DevOps/api/server.py

--- DevOps/api/server.py
+++ DevOps/api/server.py
@@ -10,7 +10,7 @@ r = redis.Redis(host=os.environ['REDIS_HOST'], port=6379, decode_responses=True)
 @app.route("/get_drinks/<ingredient>", methods=["GET"])
 def get_drinks(ingredient):
     ingredient = ingredient.lower()
-    list_drinks = []
+    dic_drinks = {}
     drinks = r.hget("INGREDIENTS", ingredient)
     if drinks != None:
         drinks = json.loads(drinks)
@@ -21,8 +21,8 @@ def get_drinks(ingredient):
         glass = r.hget(drink, "glass")
         ingredients = json.loads(r.hget(drink, "ingredients"))
         preparation = r.hget(drink, "preparation")
-        list_drinks.append({"name": drink, "glass":glass, "ingredients":ingredients, "preparation":preparation})
-    return jsonify(list_drinks)
+        dic_drinks[drink] = {"glass":glass, "ingredients":ingredients, "preparation":preparation}
+    return jsonify(dic_drinks)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
