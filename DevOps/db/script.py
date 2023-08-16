a163a38a0eb4b17c007d7d0b552aa5ce68e87d1f
DynoW <104089032+DynoW@users.noreply.github.com>
2023-08-16 08:41:20 +0000

    modified:   DevOps/YAML/api.yaml 	new file:   DevOps/YAML/jobs.yaml 	modified:   DevOps/api/server.py 	modified:   DevOps/db/script.py

--- DevOps/db/script.py
+++ DevOps/db/script.py
@@ -2,17 +2,17 @@ import redis
 import json
 import os
 
-r = redis.Redis(host=os.environ["REDIS_HOST"], port=6379, decode_responses=True)
+r = redis.StrictRedis(host=os.environ["REDIS_HOST"], port=6379, password="Parola", decode_responses=True)
 
-f = open('Cocktails3.json')
+f = open('cocktails3.json')
 
 cocktails = json.load(f)
 
 for cocktail in cocktails:
     cocktail2 = cocktail.lower()
-    r.hmset(cocktail2, {"colors": json.dumps(cocktails[cocktail]["colors"]), "glass": cocktails[cocktail]["glass"], "ingredients": json.dumps(cocktails[cocktail]["ingredients"]), "preparation": cocktails[cocktail]["preparation"], "image":cocktails[cocktail]["image"]})
-    
-g = open('ingredients2.json')
+    r.hmset(cocktail2, {"colors": json.dumps(cocktails[cocktail]["colors"]), "glass": cocktails[cocktail]["glass"], "ingredients": json.dumps(cocktails[cocktail]["ingredients"])})
+    r.hmset(cocktail2,{ "preparation": cocktails[cocktail]["preparation"], "image":cocktails[cocktail]["image"]})
+g = open('ingredients.json')
 ingredients = json.load(g)
 
 for ingredient in ingredients:
